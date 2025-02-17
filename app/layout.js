b2ddc512c70675bcd0c56241c3f1200a0b08ea2e
guru2205coder <gururajkaki2@gmail.com>
2025-02-17 23:20:38 +0530

    Favicon update

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Outfit,Ovo } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const outfit = Outfit({
   subsets: ["latin"], weight:["400","500","600","700"]
@@ -12,11 +13,18 @@ const ovo = Ovo({
 export const metadata = {
   title: "Gururaj-Portfolio",
   description: "Welcome to my portfolio! I'm Gururaj Kaki, a passionate software developer specializing in Java, full-stack web development, and data analysis. Explore my projects, skills, and journey in building innovative applications.",
+  icons:{
+    icon:['/favicon.ico?v=4'],
+    apple:['/apple-touch-icon.png?v=4'],
+    shortcut:['apple-touch-icon.png']
+  }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth ">
+
+      
       <body
         className={`${outfit.className} ${ovo.className} antialiased leading-7 overflow-x-hidden dark:bg-darkTheme dark:text-white`}>
         {children}
